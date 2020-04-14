ca1f1498dc044b2cb7276716e2f4fd6c4901dda9
oguzaktas <oguzaktas0x@gmail.com>
2020-04-14 07:15:36 +0300

    Add HackerRank practices

--- leetcode/30-day-leetcoding-challenge/diameter_of_binary_tree.java
+++ leetcode/30-day-leetcoding-challenge/diameter_of_binary_tree.java
@@ -53,3 +53,22 @@ class Solution {
     }
 }
 */
+
+/* https://www.youtube.com/watch?v=M_BaOHkTtyY
+class Solution {
+    pair<int, int> dfs(TreeNode* root) {
+        if (root == NULL) {
+            return {0, 0};
+        }
+        pair<int, int> left = dfs(root->left);
+        pair<int, int> right = dfs(root->right);
+        // (diameter, depth)
+        int diam = max({left.first, right.first, left.second + right.second});
+        return {diam, max(left.second, right.second) + 1}
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        return dfs(root).first;
+    }
+}
+*/
