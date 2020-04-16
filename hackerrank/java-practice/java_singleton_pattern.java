0696773bef5c195ace1535388aba83d178fc08df
oguzaktas <oguzaktas0x@gmail.com>
2020-04-16 14:12:26 +0300

    Add Valid Paranthesis String problem

---
+++ hackerrank/java-practice/java_singleton_pattern.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    private static Singleton instance;
+    public static String str;
+    private Singleton() {
+        
+    }
+    
+    static Singleton getSingleInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+// https://www.hackerrank.com/challenges/java-singleton/problem
---
+++ leetcode/30-day-leetcoding-challenge/valid_paranthesis_string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int cmin = 0, cmax = 0;
+        for (char c : s) {
+            if (c == '(') {
+                cmax++;
+                cmin++;
+            }
+            if (c == ')') {
+                cmax--;
+                cmin = max(cmin - 1, 0);
+            }
+            if (c == '*') {
+                cmax++;
+                cmin = max(cmin - 1, 0);
+            }
+            if (cmax < 0) {
+                return false;
+            }
+        }
+        return cmin == 0;
+    }
+};
+
+// https://leetcode.com/problems/valid-parenthesis-string/
