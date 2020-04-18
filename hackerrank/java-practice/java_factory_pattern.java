37e4f46bedcfa7ee879c5e7d14476fae1fd8cf50
oguzaktas <oguzaktas0x@gmail.com>
2020-04-18 08:33:46 +0300

    Add files and LeetCode daily challenge

--- hackerrank/java-practice/java_factory_pattern.java
+++ hackerrank/java-practice/java_factory_pattern.java
@@ -2,78 +2,80 @@
 
 import java.util.*;
 import java.security.*;
+
 interface Food {
-	 public String getType();
-	}
-	class Pizza implements Food {
-	 public String getType() {
-	 return "Someone ordered a Fast Food!";
-	 }
-	}
-
-	class Cake implements Food {
-
-	 public String getType() {
-	 return "Someone ordered a Dessert!";
-	 }
-	}
-	class FoodFactory {
-		public Food getFood(String order) {
-
-if (order == null) {
-    return null;
-} else if (order.equalsIgnoreCase("cake")) {
-    return new Cake();
-} else if (order.equalsIgnoreCase("pizza")) {
-    return new Pizza();
-} else {
-    return null;
+    public String getType();
 }
 
-}//End of getFood method
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
 
-	}//End of factory class
+class Cake implements Food {
 
-	public class Solution {
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
 
-	 public static void main(String args[]){
-			Do_Not_Terminate.forbidExit();
+class FoodFactory {
+    public Food getFood(String order) {
 
-		try{
+        if (order == null) {
+            return null;
+        } else if (order.equalsIgnoreCase("cake")) {
+            return new Cake();
+        } else if (order.equalsIgnoreCase("pizza")) {
+            return new Pizza();
+        } else {
+            return null;
+        }
 
-			Scanner sc=new Scanner(System.in);
-			//creating the factory
-			FoodFactory foodFactory = new FoodFactory();
-	
-			//factory instantiates an object
-			Food food = foodFactory.getFood(sc.nextLine());
-	
-			
-			System.out.println("The factory returned "+food.getClass());
-			System.out.println(food.getType());
-		}
-		catch (Do_Not_Terminate.ExitTrappedException e) {
-			System.out.println("Unsuccessful Termination!!");
-		}
-	 }
+    }// End of getFood method
 
-	}
-	class Do_Not_Terminate {
-		 
-	    public static class ExitTrappedException extends SecurityException {
+}// End of factory class
 
-			private static final long serialVersionUID = 1L;
-	    }
-	 
-	    public static void forbidExit() {
-	        final SecurityManager securityManager = new SecurityManager() {
-	            @Override
-	            public void checkPermission(Permission permission) {
-	                if (permission.getName().contains("exitVM")) {
-	                    throw new ExitTrappedException();
-	                }
-	            }
-	        };
-	        System.setSecurityManager(securityManager);
-	    }
-	}	
+public class Solution {
+
+    public static void main(String args[]) {
+        Do_Not_Terminate.forbidExit();
+
+        try {
+
+            Scanner sc = new Scanner(System.in);
+            // creating the factory
+            FoodFactory foodFactory = new FoodFactory();
+
+            // factory instantiates an object
+            Food food = foodFactory.getFood(sc.nextLine());
+
+            System.out.println("The factory returned " + food.getClass());
+            System.out.println(food.getType());
+        } catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+}
+
+class Do_Not_Terminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
---
+++ leetcode/30-day-leetcoding-challenge/number_of_islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        if (grid.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+}
+
+// https://leetcode.com/problems/number-of-islands/
